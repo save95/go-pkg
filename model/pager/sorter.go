143afba76f98bdc8dc0f61631186881b2470df98
luoyu <luoyu@medlinker.com>
2021-06-21 15:21:37 +0800

    mod: pager sorter

--- model/pager/sorter.go
+++ model/pager/sorter.go
@@ -8,6 +8,17 @@ const (
 	DESC               // 倒序
 )
 
+func (s Sorted) String() string {
+	switch s {
+	case ASC:
+		return "ASC"
+	case DESC:
+		return "DESC"
+	default:
+		return "ASC"
+	}
+}
+
 // Sorter 排序器
 type Sorter struct {
 	Field  string
