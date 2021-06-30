f8c5204b8e99974a48c820d7990120b935899d8e
luoyu <luoyu@medlinker.com>
2021-06-30 11:00:53 +0800

    mod: config constant

--- constant/config.go
+++ constant/config.go
@@ -1,7 +1,9 @@
 package constant
 
 const (
-	ConfigFilename         = "config/config.toml"         // Deprecated APP 配置文件
+	// Deprecated
+	ConfigFilename = "config/config.toml" // APP 配置文件
+
 	ExampleConfigFilename  = "config/config.example.toml" // APP 配置样例文件
 	ExampleLocalDBFilename = "config/main.example.db"     // 本地 sqlite db 样例文件
 )
