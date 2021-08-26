a1d46475d5c8c906161f19f538c38ffc4cb6b137
luoyu <luoyu@medlinker.com>
2021-08-26 21:58:30 +0800

    mod: jwt token maker add: auto refresh token middleware

--- http/jwt/claims.go
+++ http/jwt/claims.go
@@ -10,9 +10,9 @@ type claims struct {
 	jwt.StandardClaims
 
 	Account string                 `json:"account,omitempty"` // 账号
-	UserID  uint                   `json:"uid"`               // 用户ID
-	Name    string                 `json:"name"`              // 姓名
-	Roles   []string               `json:"roles"`             // 角色组
-	Ip      string                 `json:"ip"`                // 用户登录ID
+	UserID  uint                   `json:"uid,omitempty"`     // 用户ID
+	Name    string                 `json:"name,omitempty"`    // 姓名
+	Roles   []string               `json:"roles,omitempty"`   // 角色组
+	Ip      string                 `json:"ip,omitempty"`      // 用户登录ID
 	Extend  map[string]interface{} `json:"extend,omitempty"`  // 扩展信息
 }
