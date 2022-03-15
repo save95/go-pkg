4d6ef47bd71bd919510b15705aad942b0a5bc012
luoyu <luoyu@medlinker.com>
2022-03-15 15:47:59 +0800

    add: [http] restful ignore path middleware

--- http/middleware/restful.go
+++ http/middleware/restful.go
@@ -24,6 +24,33 @@ func RESTFul(version types.ApiVersion) gin.HandlerFunc {
 	}
 }
 
+type IgnorePath struct {
+	Path   string
+	Method string
+}
+
+// RESTFulWithIgnores 忽略指定 path 的Restful 标准检测解析中间件
+// 一般，用在部分直接下载或浏览器直接访问的接口
+func RESTFulWithIgnores(version types.ApiVersion, ignorePaths ...IgnorePath) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		for _, ignore := range ignorePaths {
+			if ignore.Path == ctx.FullPath() &&
+				strings.ToLower(ctx.Request.Method) == strings.ToLower(ignore.Method) {
+				ctx.Next()
+				return
+			}
+		}
+
+		if err := (restful{ctx: ctx, version: version}).Handle(); nil != err {
+			fmt.Printf("not support accept: %s\n", err)
+			_ = ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("not support accept"))
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
 type restful struct {
 	ctx     *gin.Context
 	version types.ApiVersion
